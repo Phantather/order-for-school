136927b6469829af0436f7e06469dd3b1f6e16cc
Phantather <akdil.hstech@gmail.com>
2023-02-14 21:48:23 +0600

    new site

--- src/js/main.js
+++ src/js/main.js
@@ -10,28 +10,38 @@ $(".header__list").on("click", function () {
     $(".header__link").removeClass("show-header__link");
 });
 
-$('#demoDefaultSelection').ddslick({
+// $('#demoDefaultSelection').ddslick({
+//
+//     onSelected: function(selectedData){
+//         //callback function: do something with selectedData;
+//     }
+// });
 
-    onSelected: function(selectedData){
-        //callback function: do something with selectedData;
-    }
-});
 
+// var swiper = new Swiper(".mySwiper", {
+//     autoplay: {
+//         delay: 2000,
+//     },
+//     loop: true,
+//     grabCursor: true,
+//     effect: "creative",
+//     creativeEffect: {
+//         prev: {
+//             shadow: true,
+//             translate: [0, 0, -400],
+//         },
+//         next: {
+//             translate: ["100%", 0, 0],
+//         },
+//     },
+// });
 
 var swiper = new Swiper(".mySwiper", {
-    autoplay: {
-        delay: 2000,
-    },
+    slidesPerView: 3,
+    spaceBetween: 30,
     loop: true,
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-        prev: {
-            shadow: true,
-            translate: [0, 0, -400],
-        },
-        next: {
-            translate: ["100%", 0, 0],
-        },
+    autoplay:{
+        delay: 2000
     },
+    speed: 1500
 });
